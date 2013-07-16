a452ea0375af6f06ba8e65f5d002fe4e5ccb8768
Laurynas Verzukauskas <laurynas.verzukauskas@nfq.lt>
2013-07-16 18:17:37 +0300

    Moved files away from deprecated dir

--- metadata.php
+++ metadata.php
@@ -24,22 +24,22 @@ $aModule = array(
         array(
             'template' => 'page/details/inc/productmain.tpl',
             'block' => 'details_productmain_zoom',
-            'file' => '/out/blocks/hotoffer_productmain.tpl',
+            'file' => '/views/blocks/hotoffer_productmain.tpl',
         ),
         array(
             'template' => 'widget/product/listitem_infogrid.tpl',
             'block' => 'widget_product_listitem_infogrid_gridpicture',
-            'file' => '/out/blocks/hotoffer_infogrid.tpl',
+            'file' => '/views/blocks/hotoffer_infogrid.tpl',
         ),
         array(
             'template' => 'widget/product/listitem_grid.tpl',
             'block' => 'widget_product_listitem_grid',
-            'file' => '/out/blocks/hotoffer_grid.tpl',
+            'file' => '/views/blocks/hotoffer_grid.tpl',
         ),
         array(
             'template' => 'widget/product/listitem_line.tpl',
             'block' => 'widget_product_listitem_line_picturebox',
-            'file' => '/out/blocks/hotoffer_list.tpl',
+            'file' => '/views/blocks/hotoffer_list.tpl',
         ),
     ),
     'files' => array(
@@ -47,7 +47,9 @@ $aModule = array(
         'hotoffer_tab' => 'Nfq/HotOffer/Controllers/Admin/HotOffer_Tab.php',
         'HotOfferOxArticle' => 'Nfq/HotOffer/Models/HotOfferOxArticle.php',
     ),
-    'settings' => array(),
+    'settings' => array(
+        array('group' => 'main', 'name' => 'sCategory', 'type' => 'str', 'value' => '32'),
+    ),
     'templates' => array(
         'hotoffer_tab.tpl' => 'Nfq/HotOffer/out/admin/tpl/hotoffer_tab.tpl',
     ),
---
+++ views/admin/en/hotoffer_lang.php
@@ -0,0 +1,13 @@
+<?php
+$sLangName = "English";
+// -------------------------------
+// RESOURCE IDENTIFIER = STRING
+// -------------------------------
+$aLang = array(
+    'charset' => 'UTF-8',
+    'GENERAL_HOT_OFFER' => 'Hot Offer',
+    'HOT_OFFER_SAVE' => 'Save',
+    'tbclhotoffer_tab' => 'Hot Offer 3',
+    'SHOP_MODULE_GROUP_main' => 'General',
+    'SHOP_MODULE_sCategory' => 'Hot Offers category ID',
+);
